1a8efd2559708de892a43387f29bf74c4647084b
nathanwag15 <natewagstaff21@gmail.com>
2026-05-16 16:16:38 -0600

    home page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//   res.send('Welcome to the home page!');
-// });
+router.get('/', (req, res) => {
+  res.send('Welcome to the home page!');
+});
 
 router.use('/contacts', require('./contacts'));
 
